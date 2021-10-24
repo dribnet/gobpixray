84e4b0155511af0dd26bb052923244bb32b89139
Tom White <tom@sixdozen.com>
2021-10-25 02:51:33 +1300

    state tracking up to mint

--- genesis/mint.js
+++ genesis/mint.js
@@ -1,15 +1,26 @@
 const GETHANDLE = 'https://data.metropolis.drib.net/get_handle'
+let syncButton = null;
+let mintButton = null;
 
 function setup() {
   print("SETTING UP!")
-  frameRate(4);
+  frameRate(1);
 
   window.onhashchange = locationHashChanged;
 
   syncButton = createButton('');
   syncButton.mousePressed(syncUnsyncPressed);
   syncButton.parent('ui_sync');
+
+  mintButton = createButton('mint');
+  mintButton.mousePressed(mintPressed);
+  mintButton.parent('ui_sync');
+  mintButton.attribute('disabled', '');
+
   updateSyncUnsyncButton();
+}
+
+function mintPressed() {
 }
 
 function syncUnsyncPressed() {
@@ -52,8 +63,10 @@ async function fetchHandleFromAddress() {
   }
 }
 
+let is_synced = false;
 function updateSyncUnsyncButton() {
   if(window.tz != null && window.tz.address != null) {
+    is_synced = true;
     syncButton.elt.innerText = 'unsync';
     if (window.tz.handle == null) {
       fetchHandleFromAddress();
@@ -63,15 +76,29 @@ function updateSyncUnsyncButton() {
     }
   }
   else if (window.tz != null && window.tz.address == null) {
+    is_synced = false;
     syncButton.elt.innerText = 'sync';
   }
   else {
+    is_synced = false;
     syncButton.elt.innerText = '?';
   }
+  updateMintButton();
+}
+
+function updateMintButton() {
+  if(is_synced && result_is_ready) {
+    // Re-enable the button
+    mintButton.removeAttribute('disabled');
+  }
+  else {
+    // Disable the button
+    mintButton.attribute('disabled', '');
+  }  
 }
 
 function draw() {
-  print("Drawing...");
+  // print("Drawing...");
 }
 
 function locationHashChanged() {
@@ -98,7 +125,7 @@ function toggleMarket() {
   }
 }
 
-help_is_shown = false;
+let help_is_shown = false;
 function toggleHelp() {
   let target = document.getElementById("help");
   if (help_is_shown) {
@@ -113,10 +140,37 @@ function toggleHelp() {
   }
 }
 
-function respondToMessage(d) {
-  console.log(d);
+
+let result_is_ready = false;
+let standby_image = "placeholder.png";
+function refreshResult(new_image) {
+  let target = document.getElementById("result_img");
+  if (new_image == null) {
+    result_is_ready = false;
+    target["src"] = standby_image;
+  }
+  else {
+    result_is_ready = true;
+    target["src"] = new_image;
+  }
+  updateMintButton();
+  // console.log("UPDATED TO " + new_image);
 }
 
-// window.addEventListener("message", (event) => {
-//   respondToMessage(event.data);
-// })
+function respondToMessage(d) {
+  // console.log(d);
+  if (!("name" in d) || (d["name"] != "prediction")) {
+    // console.log("NO PREDICTION")
+    return;
+  }
+  if (d["data"]["status"] == "success") {
+    refreshResult("https://replicate.ai" + d["data"]["output_file"]);
+  }
+  else if (result_is_ready) {
+    refreshResult(null);
+  }
+}
+
+window.addEventListener("message", (event) => {
+  respondToMessage(event.data);
+})
